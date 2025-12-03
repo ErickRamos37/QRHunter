32ab40f022e05d55498ea79f671ca5bcd647f61a
Erick Ramos <erick.ramos37@uabc.edu.mx>
2025-12-02 23:36:41 -0800

    implementación del las rutas centralizadas frontend en header.php

--- views/header.php
+++ views/header.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html lang="es">
 
+<?php
+    require_once("../../config/config.php");
+    require_once(RUTA_RAIZ."/config/conexion.php"); 
+    require_once(RUTA_RAIZ."/views/header.php");
+    $conn = conectarBD();
+?>
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>QR Hunter - Navegación</title>
-    <link rel="stylesheet" href="css/StylesHeader.css">
+    <link rel="stylesheet" href="<?php echo RUTA_CSS?>StylesHeader.css">
 </head>
 
 <body>
@@ -12,23 +19,22 @@
         <h1 class="titleheader">QR Hunter</h1>
         <nav>
             <ul class="nav-links">
-                <li><a href="../views/ranking.php">Ranking</a></li>
-                <li><a href="../views/equipos.php">Equipos</a></li>
-                <li><a href="../views/escuelas.php">Escuelas</a></li>
-                <li><a href="../views/ciudades.php">Ciudades</a></li>
-                <li><a href="../views/dispositivos.php">Dispositivos</a></li>
-                <li><a href="../views/qr.php">QR</a></li>
+                <li><a href="<?php echo BASE_URL?>views/ranking.php">Ranking</a></li>
+                <li><a href="<?php echo BASE_URL?>GRUD/Leer/listaEquipos.php">Equipos</a></li>
+                <li><a href="<?php echo BASE_URL?>views/escuelas.php">Escuelas</a></li>
+                <li><a href="<?php echo BASE_URL?>views/ciudades.php">Ciudades</a></li>
+                <li><a href="<?php echo BASE_URL?>views/dispositivos.php">Dispositivos</a></li>
+                <li><a href="<?php echo BASE_URL?>views/qr.php">QR</a></li>
 
                 <li class="dropdown">
                     <button class="dropbtn"><?php echo isset($_SESSION['admin']) ? $_SESSION['admin'] : 'Usuario'; ?> &#x25BC;</button>
                     <div class="dropdown-content">
-                        <a href="indexAdmin.php">Administradores</a>
-                        <a href="logout.php">Cerrar Sesión</a>
+                        <a href="<?php echo BASE_URL?>views/indexAdmin.php">Administradores</a>
+                        <a href="<?php echo BASE_URL?>views/logout.php">Cerrar Sesión</a>
                     </div>
                 </li>
             </ul>
         </nav>
     </header>
     </body>
-
 </html>
