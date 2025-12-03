aeb1bf93edb495b0e00af820ba5af2e61ad1f7c8
Erick Ramos <erick.ramos37@uabc.edu.mx>
2025-12-02 23:15:13 -0800

    Agregar archivo config.php para gestión centralizada de rutas.

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+    // Definicion de la Ruta Absoluta para backend
+    define("RUTA_RAIZ", dirname(__DIR__));
+    // Definicion de la Ruta para frontend
+    define("BASE_URL", "/QRHunter/");
+    define("RUTA_CSS", "/QRHunter/views/css/");
+?>
