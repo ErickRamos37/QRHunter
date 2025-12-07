127b21f475593dfe72997c9bff5ad7b6ba5755c5
ErickRamos37 <erick.ramos37@uabc.edu.mx>
2025-12-06 18:51:28 -0800

    Create dashboard.php
    Primera versión del dashboard

---
+++ views/dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php
+    require_once("../config/config.php");
+    require_once(RUTA_RAIZ."/config/conexion.php"); 
+    require_once(RUTA_RAIZ."/views/header.php");
+    $conn = conectarBD();
+?>
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>QR Hunter - Dashboard</title>
+        <link rel="stylesheet" href="<?php echo RUTA_CSS?>styles.css">
+    </head>
+    <body>
+        <div>
+
+        </div>
+    </body>
+</html>
