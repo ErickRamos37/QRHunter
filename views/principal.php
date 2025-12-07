ac5fe527568f98c84b05c3a2454fa9a68a7cb617
ErickRamos37 <erick.ramos37@uabc.edu.mx>
2025-12-06 16:46:33 -0800

    Revert "Solución de error en header y inclucion de config.php en el archivo principal.php"
    This reverts commit 792f8f53917272386d08ee758650f748acc82d24.

--- views/principal.php
+++ views/principal.php
@@ -1,26 +1,20 @@
 <?php
 session_start();
 
-    require_once("../config/config.php");
-    require_once(RUTA_RAIZ."/config/conexion.php"); 
-    require_once(RUTA_RAIZ."/views/header.php");
-    $conn = conectarBD();
-
-// Verificar que el admin haya iniciado sesiï¿½n
+// Verificar que el admin haya iniciado sesión
 if (!isset($_SESSION['admin'])) {
     header("Location: ../loginAdmin.php");
     exit();
 }
 
-
 // Incluir header con navbar
-// require("header.php");
+require("header.php");
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>ï¿½rea de Administrador - QR Hunter</title>
+    <title>Área de Administrador - QR Hunter</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
@@ -32,7 +26,7 @@ if (!isset($_SESSION['admin'])) {
     </div>
 
     <script>
-        // Lï¿½gica para el Cierre de Sesiï¿½n usando PHP
+        // Lógica para el Cierre de Sesión usando PHP
         const logoutBtn = document.querySelector('nav a[href="../logout.php"], nav a[href="#"]');
 
         if(logoutBtn){
