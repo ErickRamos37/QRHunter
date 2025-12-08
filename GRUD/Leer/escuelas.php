4c6788fcdbe16420420baec40ff665940c43b4c0
Alansando121 <158650490+Alansando121@users.noreply.github.com>
2025-12-07 23:43:47 -0800

    eliminar

--- GRUD/Leer/escuelas.php
+++ GRUD/Leer/escuelas.php
@@ -47,10 +47,10 @@
                             <td><?php echo htmlspecialchars($escuela["nombre"]) ?></td>
                             <td><?php echo htmlspecialchars($escuela["idciudad"]) ?></td>
                             <td><?php echo htmlspecialchars($escuela["fecha_registro"]) ?></td>
-                            <td>
-                               <a href="<?php echo BASE_URL?>GRUD/Actualizar/formularioEditarEscuela.php?id_escuela=<?php echo $escuela["id_escuela"] ?>"><button class="buttonEditar">Editar</button></a>
+                           <td>
+                                <a href="<?php echo BASE_URL?>GRUD/Actualizar/formularioEditarEscuela.php?id_escuela=<?php echo $escuela["id_escuela"] ?>"><button class="buttonEditar">Editar</button></a>
                                 
-                               <a href="<?php echo BASE_URL?>GRUD/Actualizar/formularioEditarEscuela.php?id_escuela=<?php echo $escuela['id_escuela'] ?>"
+                                <a href="<?php echo BASE_URL?>GRUD/Eliminar/eliminarEscuela.php?id_escuela=<?php echo $escuela['id_escuela'] ?>"
                                     onclick="return confirm('¿Estás seguro de que quieres eliminar esta escuela?');"><button class="buttonEliminar">Eliminar</button></a>
                             </td>
                         </tr>
