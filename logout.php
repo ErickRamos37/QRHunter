f86c3e776da6a3df5677f1469ecbf64c853ec31b
Alfredo Gallardo <alfredogallardo778@gmail.com>
2025-12-07 17:49:38 -0800

    Update logout.php
    logout ya funciona

--- logout.php
+++ logout.php
@@ -1,19 +1,10 @@
 <?php
 session_start();
 
-$_SESSION = array();
-
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-
-// Destruye la sesi�n
+// Destruye la sesion
 session_destroy();
 
 // Redirige al login
-header("Location: /loginAdmin.php");
+header("Location: /QRHunter/loginAdmin.php");
 exit();
+?>
