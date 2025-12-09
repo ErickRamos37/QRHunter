19feb9322bcaf304f047cf9c6876cde8459e6236
Alansando121 <158650490+Alansando121@users.noreply.github.com>
2025-12-09 01:25:05 -0800

    Regreso a original

--- GRUD/Leer/escuelas.php
+++ GRUD/Leer/escuelas.php
@@ -26,11 +26,7 @@
         <h2>Lista de Escuelas Registradas</h2>
         
         <div class="header-acciones" style="justify-content: flex-start; margin-bottom: 20px;">
-<<<<<<< HEAD
             <a href="<?php echo BASE_URL?>Formularios/crearEscuela.php"><button class="buttonNormal"> Agregar Nueva Escuela</button></a>
-=======
-            <a href="<?php echo BASE_URL?>Formularios/crearEscuela.php"><button class="buttonNormal">+ Agregar Nueva Escuela</button></a>
->>>>>>> 905f7c003abe835186b39c04830c22813eb736f1
         </div>
         
         <table id="tablaEscuelas" class="display"> 
@@ -83,11 +79,11 @@
         $(document).ready(function() {
             $('#tablaEscuelas').DataTable({
                 "language": {
-                    "lengthMenu": "Mostrar _MENU_ registros",
+                    "lengthMenu": "Mostrar MENU registros",
                     "zeroRecords": "No se encontraron resultados",
-                    "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_",
+                    "info": "Mostrando registros del START al END de un total de TOTAL",
                     "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0",
-                    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                    "infoFiltered": "(filtrado de un total de MAX registros)",
                     "sSearch": "Buscar:",
                     "oPaginate": {
                         "sFirst": "Primero",
